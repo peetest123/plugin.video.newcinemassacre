ff3a4d2d635e903bf23a5e76fc77bf5a384a5452
input image inc <philipbartom@gmail.com>
2016-11-04 14:11:57 -0230

    Add files via upload

--- default.py
+++ default.py
@@ -19,8 +19,16 @@ icon = local.getAddonInfo('icon')
 YOUTUBE_CHANNEL_ID_1 = "JamesNintendoNerd"
 YOUTUBE_CHANNEL_ID_2 = "Cinemassacre"
 YOUTUBE_CHANNEL_ID_3 = "PL2B009153AC977F90"
+YOUTUBE_CHANNEL_ID_4 = "PLbQ-gSLYQEc7NPEexZVMKfvDJinOsL7YF"
+YOUTUBE_CHANNEL_ID_5 = "PLbQ-gSLYQEc4Ah-5yF3IH29er13oJs_Xy"
+YOUTUBE_CHANNEL_ID_6 = "PLb8K22b6dc0xc8K2VHDGj7LeNLdbPEbN_"
+YOUTUBE_CHANNEL_ID_7 = "PLb8K22b6dc0xuu7OXxc6MYRtaZ7AQxveW"
+YOUTUBE_CHANNEL_ID_8 = "PLb8K22b6dc0z3w9sXD2-aTlapcK0odEZ2"
+YOUTUBE_CHANNEL_ID_9 = "PLb8K22b6dc0wo6jhgOnIVeAXgAKXgAM4l"
 
 # Entry point
+
+
 def run():
     plugintools.log("FULLYCHARGEDSHOW.run")
     
@@ -36,12 +44,13 @@ def run():
     plugintools.close_item_list()
 
 # Main menu
+
 def main_list(params):
     plugintools.log("FULLYCHARGEDSHOW.main_list "+repr(params))
 
     plugintools.add_item( 
         #action="", 
-        title="Cinemassacre Channel",
+        title="The Latest Videos",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_1+"/",
         thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/latest%20videos.png",
         folder=True )
@@ -59,6 +68,49 @@ plugintools.add_item(
         url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_3+"/",
         thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/angry2.png",
         folder=True )
+
+plugintools.add_item( 
+        #action="", 
+        title="Board James",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_4+"/",
+        thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/bj.png",
+        folder=True )
+
+plugintools.add_item( 
+        #action="", 
+        title="You Know Whats Bullshit ?",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_5+"/",
+        thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/bullshit.png",
+        folder=True )
+plugintools.add_item( 
+        #action="", 
+        title="Behind The Scenes",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_6+"/",
+        thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/bts.png",
+        folder=True )
+plugintools.add_item( 
+        #action="", 
+        title="Movies",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_7+"/",
+        thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/movies.png",
+        folder=True )
+plugintools.add_item( 
+        #action="", 
+        title="Reviews",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_8+"/",
+        thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/bullshit.png",
+        folder=True )
+plugintools.add_item( 
+        #action="", 
+        title="Extra Shit :)",
+        url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID_9+"/",
+        thumbnail="https://dl.dropboxusercontent.com/u/50103581/avgn%20addon/extra.png",
+        folder=True )
+		
+		
+		
 		
 
 run()
+
+
